598bc2d59a59654dac8fbafda7b58d65645432ec
PrasannaJung <prasannajung08@gmail.com>
2023-06-05 18:38:57 +0545

    Initial Commit

---
+++ src/main/java/com/kaamkuro/kaamkuro/KaamkuroApplication.java
@@ -0,0 +1,13 @@
+package com.kaamkuro.kaamkuro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class   KaamkuroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KaamkuroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kaamkuro/kaamkuro/KaamkuroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kaamkuro.kaamkuro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KaamkuroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
