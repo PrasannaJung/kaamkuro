26ce86a3a8cfc95162062649c61a4b67a6bd15fb
PrasannaJung <prasannajung08@gmail.com>
2023-07-02 10:15:51 +0545

    Signed up user to the database

--- src/main/java/com/kaamkuro/kaamkuro/controller/SignupController.java
+++ src/main/java/com/kaamkuro/kaamkuro/controller/SignupController.java
@@ -1,25 +1,28 @@
 package com.kaamkuro.kaamkuro.controller;
 
 import com.kaamkuro.kaamkuro.dto.NewUser;
+import com.kaamkuro.kaamkuro.service.AuthUserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
+@RequiredArgsConstructor
 public class SignupController {
 
-    @GetMapping("/signup")
-    public String getSignupPage(){
-        return "signup";
-    }
+    private final AuthUserService authUserService;
 
-    @PostMapping("/save")
+    @PostMapping("/signup/save")
     public String postUser(@ModelAttribute NewUser newUser){
+        System.out.println("Signup controller hit");
         System.out.println(newUser.getEmail());
         System.out.println(newUser.getPassword());
         System.out.println(newUser.getRole());
-        return "redirect:/signup";
+        authUserService.saveUser(newUser);
+
+        return "redirect:/login";
     }
 
 }
