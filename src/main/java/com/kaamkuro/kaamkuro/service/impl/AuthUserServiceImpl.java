9047084fb7df9c75e9884fa430b2a576a4650e8a
PrasannaJung <prasannajung08@gmail.com>
2023-07-02 10:16:19 +0545

    Added backend logic to register user

--- src/main/java/com/kaamkuro/kaamkuro/service/impl/AuthUserServiceImpl.java
+++ src/main/java/com/kaamkuro/kaamkuro/service/impl/AuthUserServiceImpl.java
@@ -1,15 +1,24 @@
 package com.kaamkuro.kaamkuro.service.impl;
 
+import com.kaamkuro.kaamkuro.config.PasswordEncoderUtil;
 import com.kaamkuro.kaamkuro.dto.NewUser;
 import com.kaamkuro.kaamkuro.entity.AuthUser;
+import com.kaamkuro.kaamkuro.repo.AuthUserRepo;
 import com.kaamkuro.kaamkuro.service.AuthUserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
+@Service
+@RequiredArgsConstructor
 public class AuthUserServiceImpl implements AuthUserService {
+    private final AuthUserRepo authUserRepo;
     @Override
     public void saveUser(NewUser newUser) {
         AuthUser user = new AuthUser();
         user.setEmail(newUser.getEmail());
         user.setRole(newUser.getRole());
-        user.setPassword(user.getPassword());
+        user.setPassword(PasswordEncoderUtil.getInstance().encode(newUser
+                .getPassword()));
+        authUserRepo.save(user);
     }
 }
