722cb6a08cefc6bcdf369fab01e59c50ddf260b5
yusha28 <neupaneyusha@gmail.com>
2023-07-06 08:19:53 +0545

    delete listing added

--- src/main/java/com/kaamkuro/kaamkuro/KaamkuroApplication.java
+++ src/main/java/com/kaamkuro/kaamkuro/KaamkuroApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class KaamkuroApplication {
+public class   KaamkuroApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(KaamkuroApplication.class, args);
 	}
