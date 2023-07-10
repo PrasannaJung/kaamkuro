57688cc0dc6ba4cd1ba879c76b894d8501b62cc5
PrasannaJung <prasannajung08@gmail.com>
2023-07-10 10:06:34 +0545

    Implemented authentication provider and done its configuration

--- src/main/java/com/kaamkuro/kaamkuro/security/SecurityConfig.java
+++ src/main/java/com/kaamkuro/kaamkuro/security/SecurityConfig.java
@@ -1,15 +1,38 @@
 package com.kaamkuro.kaamkuro.security;
 
 
+import com.kaamkuro.kaamkuro.dto.CustomUserDetails;
+import com.kaamkuro.kaamkuro.service.impl.CustomUserDetailsService;
+import com.kaamkuro.kaamkuro.utils.PasswordEncoderUtil;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final CustomUserDetailsService customUserDetailsService;
+
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+        daoAuthenticationProvider.setUserDetailsService(customUserDetailsService);
+        daoAuthenticationProvider.setPasswordEncoder(PasswordEncoderUtil.getInstance());
+        return daoAuthenticationProvider;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws  Exception{
         httpSecurity.csrf().disable()
