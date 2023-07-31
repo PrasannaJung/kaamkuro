33b4af6ccf89cb2e3bff5fa5cf3a8fba068a245b
SuyogManandhar <skmanandhar803@gmail.com>
2023-07-31 23:17:08 +0545

    create full database for job lisiting

--- src/main/java/com/kaamkuro/kaamkuro/dto/JobDto.java
+++ src/main/java/com/kaamkuro/kaamkuro/dto/JobDto.java
@@ -1,5 +1,7 @@
 package com.kaamkuro.kaamkuro.dto;
 
+import com.kaamkuro.kaamkuro.entity.Job;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,7 +12,28 @@ import lombok.Setter;
 @AllArgsConstructor
 @NoArgsConstructor
 public class JobDto {
+    private Integer id;
+
+    @NotEmpty(message = "name can't be empty")
     private String jobname;
-    private String description;
+
+    @NotEmpty (message = "position can't be empty")
+    private String jobPosition;
+
     private Integer Salary;
+
+    @NotEmpty (message = "companyName can't be empty")
+    private String companyName;
+
+    @NotEmpty (message = "jobType can't be empty")
+    private String jobType;
+
+    public JobDto(Job job){
+        this.id = job.getId();
+        this.jobname = job.getJobname();
+        this.jobPosition = job.getJobPosition();
+        this.Salary = job.getSalary();
+        this.companyName = job.getCompanyName();
+        this.jobType = job.getJobType();
+    }
 }
--- src/main/java/com/kaamkuro/kaamkuro/entity/Job.java
+++ src/main/java/com/kaamkuro/kaamkuro/entity/Job.java
@@ -2,10 +2,7 @@ package com.kaamkuro.kaamkuro.entity;
 
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.util.List;
 
@@ -13,6 +10,7 @@ import java.util.List;
 @Table
 @Getter
 @Setter
+@Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class Job {
