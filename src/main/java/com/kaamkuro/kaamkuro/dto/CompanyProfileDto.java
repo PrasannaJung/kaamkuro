fe8064becdf3119222c4593d636f3f0e83425a05
nihistha <nihirastha20@gmail.com>
2023-07-31 10:00:55 +0545

    controller and dto fixed

--- src/main/java/com/kaamkuro/kaamkuro/dto/CompanyProfileDto.java
+++ src/main/java/com/kaamkuro/kaamkuro/dto/CompanyProfileDto.java
@@ -3,5 +3,6 @@ package com.kaamkuro.kaamkuro.dto;
 public class CompanyProfileDto {
     public String name;
     public String address;
-    public
+    public String number;
+    public String email;
 }
