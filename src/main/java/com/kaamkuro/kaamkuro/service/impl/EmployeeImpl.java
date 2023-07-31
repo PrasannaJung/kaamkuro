233bb97dee6a69fd1467ed691d60d528ea79723b
nihistha <nihirastha20@gmail.com>
2023-07-31 19:34:55 +0545

    emp impl

---
+++ src/main/java/com/kaamkuro/kaamkuro/service/impl/EmployeeImpl.java
@@ -0,0 +1,11 @@
+package com.kaamkuro.kaamkuro.service.impl;
+
+import com.kaamkuro.kaamkuro.dto.EmployeeDto;
+import com.kaamkuro.kaamkuro.service.EmployeeService;
+
+public class EmployeeImpl implements EmployeeService {
+    @Override
+    public void addEmployee(EmployeeDto employeeDto) {
+        
+    }
+}
