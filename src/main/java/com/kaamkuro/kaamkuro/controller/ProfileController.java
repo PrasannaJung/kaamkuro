1c3af8f644fc9058f5fef147d51d3055052499a3
PrasannaJung <prasannajung08@gmail.com>
2023-08-01 10:09:41 +0545

    Made a controller function to update employee data

--- src/main/java/com/kaamkuro/kaamkuro/controller/ProfileController.java
+++ src/main/java/com/kaamkuro/kaamkuro/controller/ProfileController.java
@@ -1,5 +1,6 @@
 package com.kaamkuro.kaamkuro.controller;
 
+import com.kaamkuro.kaamkuro.dto.EmployeeDto;
 import com.kaamkuro.kaamkuro.entity.Employee;
 import com.kaamkuro.kaamkuro.service.EmployeeService;
 import lombok.RequiredArgsConstructor;
@@ -8,6 +9,8 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 @RequiredArgsConstructor
@@ -21,5 +24,23 @@ public class ProfileController {
         System.out.println(profile.getEmail());
         model.addAttribute("profile",profile);
         return "Employee profile";
+    }
+    @PostMapping("/employee/profile/update")
+    public String updateProfile(@ModelAttribute EmployeeDto employeeDto){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String email = authentication.getName();
+        Employee existingProfile = employeeService.getEmployee(email);
+        existingProfile.setEmail(employeeDto.getEmail());
+        existingProfile.setPassword(employeeDto.getPassword());
+        existingProfile.setAge(employeeDto.getAge());
+        existingProfile.setId(employeeDto.getId());
+        existingProfile.setDesc(employeeDto.getMydescription());
+        existingProfile.setName(employeeDto.getName());
+        existingProfile.setProfession(employeeDto.getProfession());
+        existingProfile.setSkill(employeeDto.getSkill());
+        existingProfile.setMainLang(employeeDto.getLanguage());
+        employeeService.update(existingProfile);
+
+        return "redirect:/employee/profile";
     }
 }
