1ce57fe7db7f3eb65c06f72a0f1f546cf95865f1
nihistha <nihirastha20@gmail.com>
2023-08-01 14:57:00 +0545

    company impl changed

--- src/main/java/com/kaamkuro/kaamkuro/service/impl/CompanyImpl.java
+++ src/main/java/com/kaamkuro/kaamkuro/service/impl/CompanyImpl.java
@@ -2,6 +2,7 @@ package com.kaamkuro.kaamkuro.service.impl;
 
 import com.kaamkuro.kaamkuro.dto.CompanyProfileDto;
 import com.kaamkuro.kaamkuro.entity.Company;
+import com.kaamkuro.kaamkuro.repo.CompanyRepo;
 import com.kaamkuro.kaamkuro.service.CompanyService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -10,7 +11,7 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class CompanyImpl implements CompanyService {
 
-
+    private final CompanyRepo companyRepo;
     @Override
     public void addCompany(CompanyProfileDto addCompany) {
         Company company= new Company();
@@ -20,6 +21,6 @@ public class CompanyImpl implements CompanyService {
         company.setDescription(company.getDescription());
         company.setPhone(company.getPhone());
         company.setId(company.getId());
-
+        companyRepo.save(company);
     }
 }
