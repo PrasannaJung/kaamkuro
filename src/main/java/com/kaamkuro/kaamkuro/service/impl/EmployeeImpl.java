b5f14ddfb97aa7e3d97cf4a0fa9b9e4d5f5cbc82
PrasannaJung <prasannajung08@gmail.com>
2023-08-01 09:06:42 +0545

    Created employee service to be able to add employee data

--- src/main/java/com/kaamkuro/kaamkuro/service/impl/EmployeeImpl.java
+++ src/main/java/com/kaamkuro/kaamkuro/service/impl/EmployeeImpl.java
@@ -3,7 +3,9 @@ package com.kaamkuro.kaamkuro.service.impl;
 import com.kaamkuro.kaamkuro.dto.EmployeeDto;
 import com.kaamkuro.kaamkuro.entity.Employee;
 import com.kaamkuro.kaamkuro.service.EmployeeService;
+import org.springframework.stereotype.Service;
 
+@Service
 public class EmployeeImpl implements EmployeeService {
     @Override
     public void addEmployee(EmployeeDto employeeDto) {
