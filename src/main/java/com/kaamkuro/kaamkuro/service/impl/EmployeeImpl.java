67894346e2d3086214ba72f1901d20bfcd82554b
PrasannaJung <prasannajung08@gmail.com>
2023-08-01 09:10:46 +0545

    Added function to be to able to add employee data in the employee service

--- src/main/java/com/kaamkuro/kaamkuro/service/impl/EmployeeImpl.java
+++ src/main/java/com/kaamkuro/kaamkuro/service/impl/EmployeeImpl.java
@@ -3,6 +3,7 @@ package com.kaamkuro.kaamkuro.service.impl;
 import com.kaamkuro.kaamkuro.dto.EmployeeDto;
 import com.kaamkuro.kaamkuro.entity.Employee;
 import com.kaamkuro.kaamkuro.service.EmployeeService;
+import com.kaamkuro.kaamkuro.utils.PasswordEncoderUtil;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -10,5 +11,7 @@ public class EmployeeImpl implements EmployeeService {
     @Override
     public void addEmployee(EmployeeDto employeeDto) {
         Employee employee = new Employee();
+        employee.setEmail(employeeDto.getEmail());
+        employee.setPassword(PasswordEncoderUtil.getInstance().encode(employeeDto.getPassword()));
     }
 }
