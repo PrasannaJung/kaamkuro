8fe81eb7b9cff14b54c0f1566a37879d1acbccf8
nihistha <nihirastha20@gmail.com>
2023-08-04 16:09:52 +0545

    job listing controller

---
+++ src/main/java/com/kaamkuro/kaamkuro/controller/JobController.java
@@ -0,0 +1,10 @@
+package com.kaamkuro.kaamkuro.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@RequiredArgsConstructor
+public class JobController {
+    
+}
